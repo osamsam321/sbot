537f8f5c4dd87bc84491ef3afdfb9932c8b7fd4b
osamsam <osamsam321@gmail.com>
2024-10-06 22:08:26 -0500

    trying to make install.sh work

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 app_version=$(curl https://raw.githubusercontent.com/osamsam321/sbot/main/VERSION)
-curl -oL https://github.com/osamsam321/sbot/releases/download/$app_version/sbot_$app_version.zip
+curl -oL sbot_$app_version.zip https://github.com/osamsam321/sbot/releases/download/$app_version/sbot_$
 unzip sbot_$app_version.zip && rm -rf ~/.sbot && mv -f sbot ~/.sbot
