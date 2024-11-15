0b7f256375c04c70b5017c76baf249bb34b9104d
osamsam <osamsam321@gmail.com>
2024-11-14 23:20:48 -0600

    add optioned to auto overright files with the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 app_version=$(curl https://raw.githubusercontent.com/osamsam321/sbot/main/VERSION)
 curl -OL https://github.com/osamsam321/sbot/releases/download/$app_version/sbot_$app_version.zip
-unzip sbot_$app_version.zip && rm -rf ~/.sbot && mv -f sbot ~/.sbot
+unzip -o sbot_$app_version.zip && rm -rf ~/.sbot && mv -f sbot ~/.sbot
